0cbd8e694c8a807f5fe876b7eae72a157a2f10dc
André Formento <andreformento.sc@gmail.com>
2017-09-16 17:11:00 -0300

    Update

--- app/src/main/java/com/example/andre/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andre/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.andre.myapplication;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
         StrictMode.setThreadPolicy(policy);
-
         clickButton.setOnClickListener(new ClickRequest(this));
     }
 
